f3cca5c7ea26b54bf3fabc85b889647a57079010
Joe <joekosten@gmail.com>
2016-12-06 11:13:56 -0500

    Movie model created

---
+++ FinalProject_WhatsAPPening/FinalProject_WhatsAPPening/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject_WhatsAPPening.Models
+{
+    //Movie class will be used in Results class
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public string Length { get; set; }
+        public string ImageURL { get; set; }
+        public string Theatre { get; set; }
+        public string TheatreAddress { get; set; }
+        public int TheatreZipCode { get; set; }
+    }
+}
