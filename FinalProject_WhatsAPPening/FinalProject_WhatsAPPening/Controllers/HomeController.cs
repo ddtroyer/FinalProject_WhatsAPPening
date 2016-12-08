aae99b786b3d33b3d57f10c6d5b18c0b053c6e65
MichaelAudu <michael.audu@aya.yale.edu>
2016-12-08 14:37:11 -0500

    Results Page!

--- FinalProject_WhatsAPPening/FinalProject_WhatsAPPening/Controllers/HomeController.cs
+++ FinalProject_WhatsAPPening/FinalProject_WhatsAPPening/Controllers/HomeController.cs
@@ -112,10 +112,22 @@ namespace FinalProject_WhatsAPPening.Controllers
                 }
             }
 
+            Random rnd = new Random();
+            int restInt = rnd.Next(1, restaurants.Count());
+            int actInt = rnd.Next(1, activities.Count());
 
-            return View("Activity", activities);
+            Restaurant modelRestaurant = restaurants[restInt];
+            Activity modelActivity = activities[actInt];
+
+            ResultViewModel result = new ResultViewModel();
+
+            result.RestaurantResult = modelRestaurant;
+            result.ActivityResult = modelActivity;
+            return View("ResultTemp", result);
+
+            //return View("Activity", activities);
             //return View("RestaurantsTemp",restaurants);
-            
+
         }
 
         public ActionResult Contact()
